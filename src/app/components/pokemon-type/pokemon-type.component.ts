68b2f30bd2a41333e06f0b1644f208986a111bc2
lucas <lucaspaulom@hotmail.com>
2019-06-23 00:17:10 -0300

    Change pokemon types component

--- src/app/components/pokemon-type/pokemon-type.component.ts
+++ src/app/components/pokemon-type/pokemon-type.component.ts
@@ -1,19 +1,52 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+export interface TypesColors {
+  type: string;
+  background: string;
+  color: string;
+}
 @Component({
   selector: 'app-pokemon-type',
   templateUrl: './pokemon-type.component.html',
   styleUrls: ['./pokemon-type.component.scss']
 })
+
 export class PokemonTypeComponent implements OnInit {
 
-  @Input() type: string;
-  @Input() background: string;
-  @Input() color: string;
+  @Input() types: string[];
+  @Input() background: string = 'blue';
+  @Input() color: string = 'white';
+
+  public typesObj: TypesColors[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.types.forEach((type: string) => {
+      let color;
+      let background;
+
+      if (type === 'Eletric') {
+        background = 'yellow';
+        color = 'black';
+      } else if (type === 'Fire') {
+        background = 'orange';
+        color = 'black';
+      } else if (type === 'Grass') {
+        background = 'green';
+        color = 'white';
+      } else {
+        background = 'gray';
+        color = 'white';
+      }
+
+      const formatedType: TypesColors = {
+        type,
+        color,
+        background,
+      };
+      this.typesObj.push(formatedType);
+    });
   }
 
 }
