93f8f22d4738aa9e4498f4e77ed297e5cac7c4c2
lucas <lucaspaulom@hotmail.com>
2019-06-23 00:31:30 -0300

    pokemon number

--- src/app/components/pokemon-item/pokemon-item.component.ts
+++ src/app/components/pokemon-item/pokemon-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-item',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonItemComponent implements OnInit {
 
+  @Input() pokemonNumber: number = 0;
+
+  public formatedNumber: string = '000';
+
   constructor() { }
 
   ngOnInit() {
+    if (this.pokemonNumber < 10) {
+      this.formatedNumber = `00${this.pokemonNumber}`;
+    } else if (this.pokemonNumber < 100) {
+      this.formatedNumber = `0${this.pokemonNumber}`;
+    } else {
+      this.formatedNumber = `${this.pokemonNumber}`;
+    }
   }
 
 }
